16e5a0b10d8bbd885d8ddb766f8892903dcfbb89
gumi <suzhou.gumi@gmail.com>
2024-07-16 15:00:50 +0800

    feat: 主页

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, Card } from "antd";
+
+const Header: React.FC = () => (
+  <>
+    <Card className="m-2 flex justify-end items-center">
+      <Button>新增表单</Button>
+    </Card>
+  </>
+);
+
+export default Header;
