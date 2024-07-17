0a269aca080b2f7f5a0c0e9aa3f084865dfbc3ad
gumi <suzhou.gumi@gmail.com>
2024-07-17 17:25:10 +0800

    feat: add dnd-kit

--- src/app/formDesign/page.tsx
+++ src/app/formDesign/page.tsx
@@ -1,17 +1,77 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "antd";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import SortableItem from "@/components/SortableItem";
 
 const { Sider, Content } = Layout;
 
 export default function Page() {
+  const [items, setItems] = useState([1, 2, 3]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  const handleDragEnd = (event: { active: any; over: any }) => {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+
   return (
     <>
-      <Layout>
-        <Sider />
-        <Content />
-      </Layout>
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <Layout>
+          <Sider>
+            <SortableContext
+              items={items}
+              strategy={verticalListSortingStrategy}
+            >
+              {items.map((id) => (
+                <SortableItem key={id} id={id} />
+              ))}
+            </SortableContext>
+          </Sider>
+          <Content>
+            <SortableContext
+              items={items}
+              strategy={verticalListSortingStrategy}
+            >
+              {items.map((id) => (
+                <SortableItem key={id} id={id} />
+              ))}
+            </SortableContext>
+          </Content>
+        </Layout>
+      </DndContext>
     </>
   );
 }
---
+++ src/components/SortableItem.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+const SortableItem = (props: { id: string }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <>
+      <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+        组件
+      </div>
+    </>
+  );
+};
+
+export default SortableItem;
